f60e6dd1930bba1ddeaab39a3861bc0d39339fdb
ion-off-grid <131008355+ion-off-grid@users.noreply.github.com>
2025-06-30 16:36:09 +0300

    Update constants.ts

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -5,7 +5,7 @@ You can add these items to the cart by clicking the cart icon on the item cards.
 As long as you haven't found these items, continue to the next page if there is one (you can see page navigation at the bottom of the page).
 Once you have added these items to the cart, go to the cart (click on the cart icon on the right of the top navbar, scroll up if you don't see it), enter shipping details with the user info and checkout.`;
 
-export const TEST_APP_URL = "http://localhost:3005";
+export const TEST_APP_URL = "https://shs.iotem.org/";
 export const USERNAME = "cua@example.com";
 export const PASSWORD = "password123";
 
